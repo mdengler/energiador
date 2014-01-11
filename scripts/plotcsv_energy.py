c913172172a32dd30c8f49e619e21c401b1a5060
Martin Dengler <martin@martindengler.com>
2014-01-11 17:40:46 +0000

    plotcsv_energy.py: use hour as xticklabel

--- scripts/plotcsv_energy.py
+++ scripts/plotcsv_energy.py
@@ -5,6 +5,7 @@
 # The first row of the csv file is assumed to contain axis labels.
 #
 import csv
+import datetime
 import os
 import sys
 
@@ -17,33 +18,34 @@ import numpy as np
 
 
 def get_data(filename):
-    t1 = []
-    t2 = []
+    times = []
     y1 = []
     y2 = []
     with open(filename, "rb") as f:
         reader = csv.reader(f, delimiter=";")
         for row in reader:
             if row[1] != "" and row[2] != "":
-                t1.append(row[0])
-                t2.append(row[0])
-                y1.append(float(row[1]))
-                y2.append(float(row[2]))
-    return t1, t2, y1, y2
+                row = map(float, row)
+                times.append(datetime.datetime.fromtimestamp(row[0]))
+                y1.append(row[1])
+                y2.append(row[2])
+    return times, y1, y2
 
 
-def make_figure(t1, t2, y1, y2):
+def make_figure(times, y1, y2):
     f, axarr = plt.subplots(3, sharex=True)
+    date_formatter = matplotlib.dates.DateFormatter("%H")
+    axarr[0].xaxis.set_major_formatter(date_formatter)
     plt.grid(True)
-    axarr[0].plot(t1[1:], np.array(y1[1:]) / 1e3, label="current")
+    axarr[0].plot(times, np.array(y1) / 1e3, label="current")
     axarr[0].set_ylabel("current [mA]")
     #plt.grid()
-    axarr[1].plot(t2[1:], np.array(y2[1:]) / 1e6, label="voltage")
+    axarr[1].plot(times, np.array(y2) / 1e6, label="voltage")
     axarr[1].set_ylabel("voltage [V]")
     #plt.grid()
-    axarr[2].plot(t2[1:], np.array(y2[1:]) / 1e6 * np.array(y1[1:]) / 1e6, label="power")
+    axarr[2].plot(times, np.array(y2) / 1e6 * np.array(y1) / 1e6, label="power")
     axarr[2].set_ylabel("power [W]")
-    plt.xlabel("time [s]")
+    plt.xlabel("time [hour]")
     #plt.grid()
     #plt.legend()
     return plt.gcf()
